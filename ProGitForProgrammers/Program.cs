338e3547dae686fba4b893dd32b38628075176b7
josephkwong <josephkwf@hotmail.com>
2022-03-13 11:26:15 +0800

    Second commit -- create the second WriteLine from command line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,6 +7,8 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Git!");
+
+Console.WriteLine("I just added this in Command Line");
         }
     }
 }
