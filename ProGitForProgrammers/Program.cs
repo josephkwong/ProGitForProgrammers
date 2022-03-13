739cb76f21831a71f654a6009bb99bf0cb1a4df1
josephkwong <josephkwf@hotmail.com>
2022-03-13 18:38:15 +0800

    Add writeline indicating we are in command line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,8 +7,8 @@ namespace ProGitForProgrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World Git!");
-
-Console.WriteLine("I just amended in VS");
+            Console.WriteLine("I just amended in VS");
+            Console.WriteLine()"I just added this in the command line repo";
         }
     }
 }
