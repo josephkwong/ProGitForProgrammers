adb5f66f47409a896fa91e2404a4bcd932a378e6
josephkwong <josephkwf@hotmail.com>
2022-03-15 22:14:21 +0800

    My message

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -10,6 +10,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("I just amended in VS");
             Console.WriteLine("I just added this in the command line repo";
             Console.WriteLine("This line added to show the staging area");
+            Console.WriteLine("This line to skipping the staging area by -a flag";
         }
     }
 }
